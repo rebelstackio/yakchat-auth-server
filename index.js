4eb792cff09441c752c294d5f8e7806ea6fda36b
Javier Galarza <jegj57@gmail.com>
2019-07-16 10:13:46 -0500

    Fixed common issue from expressif 2.x version

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ require('dotenv').config();
 require('app-module-path').addPath(__dirname);
 const pjson = require('./package.json');
 
-global.E = require('expressif');
+global.E = require('@rebelstack-io/expressif');
 // global.db = new E.DB();
-global.LOGGER = new E.Logger();
+global.LOGGER = new E.BasicLogger();
 
 const server = new E.Server(
 	{
-		"title": "Yakchat Auth REST Server",
-		"version": pjson.version,
-		"config": {
-			"port": process.env.PORT,
-			"routers":[
-				{ "relpath": "routers" }
-			]
-		}
-	},
-	{
-
+		"port": process.env.PORT,
+		"routers":[
+			{ "relpath": "routers" }
+		]
 	}
 );
 const auth = new E.Auth(process.env.JWT_SECRET);
--- routers/auth/index.js
+++ routers/auth/index.js
@@ -1,13 +1,13 @@
 /* routers/com/index.js */
 'use strict';
-const JSONValidator = require('expressif').JSONValidator;
-const Router = require('expressif').Router;
-const RX = require('expressif').ReqValidator;
+const JSONValidator = require('@rebelstack-io/expressif').JSONValidator;
+const Router = require('@rebelstack-io/expressif').Router;
+const RX = require('@rebelstack-io/expressif').ReqValidator;
 const sc = require('schemas/auth');
 const cc = require('controllers/auth');
 
 const AuthRouter = function AuthRouter(auth) {
-	let jv = new JSONValidator(sc, { allErrors: true });
+	let jv = new JSONValidator(sc, { allErrors: true, jsonPointers: true });
 	const routes = [
 		{
 			method: 'post', path: '/login', rprivs: null, mwares: [cc.login],
